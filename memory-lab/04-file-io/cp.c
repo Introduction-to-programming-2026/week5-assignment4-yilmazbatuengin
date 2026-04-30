834a920d67ef659eda5730264fb38f12e3bb42ae
yilmazbatuengin <sns6267f9s@privaterelay.appleid.com>
2026-04-30 19:07:57 +0200

    file io tested

--- memory-lab/04-file-io/cp.c
+++ memory-lab/04-file-io/cp.c
@@ -1,9 +1,3 @@
-// Copies a file byte by byte
-//
-// This shows how files are just sequences of bytes in memory.
-// We read one byte at a time from the source and write it to the destination.
-// Usage: ./cp source destination
-
 #include <stdio.h>
 
 typedef unsigned char BYTE;
@@ -16,16 +10,16 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // TODO: Open argv[1] for reading in binary mode "rb"
-    FILE *src = fopen(/* ??? */, /* ??? */);
+
+    FILE *src = fopen(argv[1], "rb");
     if (src == NULL)
     {
         printf("Could not open source file.\n");
         return 1;
     }
 
-    // TODO: Open argv[2] for writing in binary mode "wb"
-    FILE *dst = fopen(/* ??? */, /* ??? */);
+
+    FILE *dst = fopen(argv[2], "wb");
     if (dst == NULL)
     {
         printf("Could not open destination file.\n");
@@ -35,16 +29,15 @@ int main(int argc, char *argv[])
 
     BYTE b;
 
-    // TODO: Loop — read one BYTE from src, write it to dst, until src is empty
-    // Hint: fread returns the number of items read; stop when it returns 0
-    while (fread(/* ??? */, sizeof(b), 1, src) != 0)
+
+    while (fread(&b, sizeof(b), 1, src) != 0)
     {
-        fwrite(/* ??? */, sizeof(b), 1, dst);
+        fwrite(&b, sizeof(b), 1, dst);
     }
 
-    // TODO: Close both files
-    /* ??? */
-    /* ??? */
+    
+    fclose(src);
+    fclose(dst);
 
     return 0;
 }
