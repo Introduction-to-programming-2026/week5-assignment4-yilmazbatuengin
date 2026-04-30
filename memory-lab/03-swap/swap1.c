68f79847896e9f350bb8b18039b131b4be416338
yilmazbatuengin <sns6267f9s@privaterelay.appleid.com>
2026-04-30 19:01:31 +0200

    swap1 done

--- memory-lab/03-swap/swap1.c
+++ memory-lab/03-swap/swap1.c
@@ -1,12 +1,7 @@
-// Swaps two integers using pointers
-//
-// Instead of passing values, we pass the ADDRESSES of x and y.
-// The function can then follow those addresses and modify the real variables.
-
 #include <stdio.h>
 
-// TODO: Update the signature — parameters should be int* (pointers to int)
-void swap(int a, int b);
+// pointer kullanıyoruz
+void swap(int *a, int *b);
 
 int main(void)
 {
@@ -15,17 +10,15 @@ int main(void)
 
     printf("x is %i, y is %i\n", x, y);
 
-    // TODO: Pass the addresses of x and y using &
-    swap(/* ??? */, /* ??? */);
+    // adres gönderiyoruz
+    swap(&x, &y);
 
     printf("x is %i, y is %i\n", x, y);
 }
 
-// TODO: Update the signature and body to use pointers
-void swap(int a, int b)
+void swap(int *a, int *b)
 {
-    // TODO: Use *a and *b to swap the actual values in memory
-    int tmp = /* ??? */;
-    /* ??? */ = /* ??? */;
-    /* ??? */ = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
